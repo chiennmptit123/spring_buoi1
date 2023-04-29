bdcfccfcbece8801fa45806082a46ebc221e53bf
chiennm <chiennm@gmail.com>
2023-04-30 00:55:31 +0700

    CRUD done

--- spring-boot-example/src/main/java/com/example1/springbootexample/controller/SachController.java
+++ spring-boot-example/src/main/java/com/example1/springbootexample/controller/SachController.java
@@ -2,12 +2,16 @@ package com.example1.springbootexample.controller;
 
 import com.example1.springbootexample.entities.Sach;
 import com.example1.springbootexample.requests.CreateSachInput;
+import com.example1.springbootexample.requests.UpdateSachInput;
 import com.example1.springbootexample.services.SachService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 public class SachController {
@@ -17,12 +21,54 @@ public class SachController {
     }
 
     @PostMapping("/sach")
-    public ResponseEntity<Sach> createTask(@RequestBody CreateSachInput createSachInput) {
-
-        System.out.println("11111111111111111111111111");
+    public ResponseEntity<Sach> createSach(@RequestBody CreateSachInput createSachInput) {
         Sach sachCreated = sachService.create(createSachInput.toSach());
 
         return new ResponseEntity<>(sachCreated, HttpStatus.CREATED);
     }
+
+    @GetMapping("/sach")
+    public ResponseEntity<List<Sach>> allSach() {
+        List<Sach> sach = sachService.findAll();
+
+        return new ResponseEntity<>(sach, HttpStatus.OK);
+    }
+// search 1 id
+//    @GetMapping("/sach/{id}")
+//    public ResponseEntity<Sach> oneSach(@PathVariable int id) {
+//        Optional<Sach> optionalSach = sachService.findById(id);
+//
+//        if (optionalSach.isPresent()) {
+//            return new ResponseEntity<>(optionalSach.get(), HttpStatus.OK);
+//        }
+//
+//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+    @PatchMapping("/sach/{id}")
+    public ResponseEntity<Sach> updateSach(@PathVariable int id, @RequestBody UpdateSachInput updateSachInput) {
+
+        Optional<Sach> optionalSach = sachService.findById(id);
+
+        if (optionalSach.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Sach sachToUpdate = optionalSach.get();
+
+        sachToUpdate.setDescription(updateSachInput.description());
+        sachToUpdate.setDueDate(updateSachInput.dueDate());
+
+        Sach sachUpdated = sachService.update(sachToUpdate);
+
+        return new ResponseEntity<>(sachUpdated, HttpStatus.OK);
+    }
+    @DeleteMapping("/sach/{id}")
+    public ResponseEntity<Void> deleteSach(@PathVariable int id) {
+        sachService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
--- spring-boot-example/src/main/java/com/example1/springbootexample/entities/Sach.java
+++ spring-boot-example/src/main/java/com/example1/springbootexample/entities/Sach.java
@@ -2,6 +2,7 @@ package com.example1.springbootexample.entities;
 
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Table(name="sach")
 @Entity
@@ -12,6 +13,12 @@ public class Sach {
     
     @Column(unique = false, length = 200, nullable = false)
     private String name;
+
+    @Column(unique = false, length = 300, nullable = false)
+    private String description;
+
+    @Column(name = "due_date")
+    private Date dueDate;
 
     public Integer getId() {
         return id;
@@ -27,6 +34,22 @@ public class Sach {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
     }
 }
 // note enum
